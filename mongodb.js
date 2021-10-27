331f50c1ccbfb5191fdda609240a42f661f98677
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-27 11:48:13 +0100

    ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,12 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-app'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -11,7 +15,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
     db.collection('users').insertOne({
-        name:'Abhi',
+        _id:id,
+        name:'Shetty',
         age:25
     },(error,result)=>{
         if(error){
@@ -20,40 +25,45 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         console.log(result.insertedId)
     })
 
-    db.collection('users').insertMany([
-        {
-            name:'Abhinav',
-            age:26
-        },
-        {
-            name:'Gunishetty',
-            age:27
-        }
-    ],(error,result)=>{
-        if(error){
-            console.log('Unable to add')
-        }
-        console.log(result.insertedIds)
-    })
+    // db.collection('users').insertMany([
+    //     {
+    //         name:'Abhinav',
+    //         age:26
+    //     },
+    //     {
+    //         name:'Gunishetty',
+    //         age:27
+    //     }
+    // ],(error,result)=>{
+    //     if(error){
+    //         console.log('Unable to add')
+    //     }
+    //     console.log(result.insertedIds)
+    // })
 
-    db.collection('tasks').insertMany([
-        {
-            desc: 'THis is task1',
-            status: true
-        },
-        {
-            desc: 'THis is task2',
-            status: false
-        },
-        {
-            desc: 'THis is task3',
-            status: true
-        },
+    // db.collection('tasks').insertMany([
+    //     {
+    //         desc: 'THis is task1',
+    //         status: true
+    //     },
+    //     {
+    //         desc: 'THis is task2',
+    //         status: false
+    //     },
+    //     {
+    //         desc: 'THis is task3',
+    //         status: true
+    //     },
 
-    ], (error, result) => {
-        if (error) {
-            console.log('Unable to add')
-        }
-        console.log(result.insertedIds)
-    })
+    // ], (error, result) => {
+    //     if (error) {
+    //         console.log('Unable to add')
+    //     }
+    //     console.log(result.insertedIds)
+    // })
+
+
+
+
+
 })
