81d50085e59d641ac099ccec0d7d6a3c4070eb8b
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-27 15:15:06 +0100

    Updated a collection using updateOne and updateMany

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
 
-const { MongoClient, ObjectID } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-app'
 
-// const id = new ObjectID()
+// const id = new ObjectId()
 // console.log(id.id.length)
 // console.log(id.toHexString().length)
 
@@ -16,20 +16,45 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName)
 
 
-    db.collection('users').findOne({ _id: new ObjectID('617925d5db0b225962b6703c') }, (error, user) => {
-        if (error) {
-            console.log('Unable to add')
+    // db.collection('users').findOne({ _id: new ObjectId('617925d5db0b225962b6703c') }, (error, user) => {
+    //     if (error) {
+    //         console.log('Unable to add')
+    //     }
+    //     console.log(user)
+    // })
+
+    // db.collection('tasks').find({status:true}).toArray((error, tasks)=>{
+    //     console.log(tasks)
+    // })
+    // db.collection('tasks').find({status:true}).count((error, count)=>{
+    //     console.log(count)
+    // })
+
+    db.collection('users').updateOne({
+        _id: new ObjectId('61792532d7677aba67d3ab59')
+    }, {
+        // $set: {
+        //     name: 'AG'
+        // }
+        $inc: {
+            age: 20
         }
-        console.log(user)
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error);
     })
 
-    db.collection('tasks').find({status:true}).toArray((error, tasks)=>{
-        console.log(tasks)
+    db.collection('tasks').updateMany({
+        status: true
+    }, {
+        $set: {
+            status: false
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
-    db.collection('tasks').find({status:true}).count((error, count)=>{
-        console.log(count)
-    })
-
-
 
 })
