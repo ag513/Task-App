45f1fae79545e88c98f6f339a951a3a1361e1c7e
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-27 12:06:03 +0100

    findOne and find to Read/search in collection

--- mongodb.js
+++ mongodb.js
@@ -4,9 +4,9 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-app'
 
-const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString().length)
+// const id = new ObjectID()
+// console.log(id.id.length)
+// console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -14,55 +14,21 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        _id:id,
-        name:'Shetty',
-        age:25
-    },(error,result)=>{
-        if(error){
-            return console.log('unable to add user')
+
+
+    db.collection('users').findOne({ _id: new ObjectID('617925d5db0b225962b6703c') }, (error, user) => {
+        if (error) {
+            console.log('Unable to add')
         }
-        console.log(result.insertedId)
+        console.log(user)
     })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name:'Abhinav',
-    //         age:26
-    //     },
-    //     {
-    //         name:'Gunishetty',
-    //         age:27
-    //     }
-    // ],(error,result)=>{
-    //     if(error){
-    //         console.log('Unable to add')
-    //     }
-    //     console.log(result.insertedIds)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         desc: 'THis is task1',
-    //         status: true
-    //     },
-    //     {
-    //         desc: 'THis is task2',
-    //         status: false
-    //     },
-    //     {
-    //         desc: 'THis is task3',
-    //         status: true
-    //     },
-
-    // ], (error, result) => {
-    //     if (error) {
-    //         console.log('Unable to add')
-    //     }
-    //     console.log(result.insertedIds)
-    // })
-
-
+    db.collection('tasks').find({status:true}).toArray((error, tasks)=>{
+        console.log(tasks)
+    })
+    db.collection('tasks').find({status:true}).count((error, count)=>{
+        console.log(count)
+    })
 
 
 
