b10db2e97ab66cce79ccee73252c0ee240a1792a
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-31 23:57:43 +0000

    added user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'Abhinav',
+    age: 25
+})
+
+me.save().then(() => {
+    console.log(me)
+}).catch((error) => {
+    console.log(error)
 })
