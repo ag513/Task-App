8f615e9f28f9de77b558a1e82fca04205749a450
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-01 17:07:27 +0000

    added task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -11,13 +11,34 @@ const User = mongoose.model('User', {
     }
 })
 
-const me = new User({
-    name: 'Abhinav',
-    age: 25
+// const me = new User({
+//     name: 'Abhinav',
+//     age: 'Abhi'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+const Task = mongoose.model('Task', {
+    desc: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 })
 
-me.save().then(() => {
-    console.log(me)
+
+const task1 = new Task({
+    desc: 'This is a task description',
+    completed: false
+})
+
+task1.save().then(() => {
+    console.log(task1)
 }).catch((error) => {
     console.log(error)
 })
