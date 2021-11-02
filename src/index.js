b62bafd6cb5deaffdb28ec8da1e6cd42d0b6081e
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-02 18:07:19 +0000

    created POST endpoint for user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+require('./db/mongoose')
+const User = require('./models/user')
+
 const app = express()
 const port = process.env.PORT || 3001
+
+
+// Incoming request to JSON 
+app.use(express.json())
+
+
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+    user.save().then(() => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
 
 app.listen(port, () => {
     console.log('server is active on ' + port)
