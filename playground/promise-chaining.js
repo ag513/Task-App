c26821cfa157abf5d2c1481bd1f71ccbf72cf45c
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-04 15:33:41 +0000

    delete user using async-await

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,10 +3,22 @@ const User = require('../src/models/user')
 
 // 6180233f36d45286ef4e49e5
 
-User.findByIdAndUpdate('61817c28604eeb705fe36bf3', { age: 1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((count) => {
+// User.findByIdAndUpdate('61817c28604eeb705fe36bf3', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((count) => {
+//     console.log(count)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('61817c28604eeb705fe36bf3', 25).then((count) => {
     console.log(count)
 }).catch((e) => {
     console.log(e)
