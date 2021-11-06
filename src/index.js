8d0c46522b9f7f001b838d5d81e3f4e3f41fefdc
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-06 00:09:17 +0000

    Created UPDATE endpoint for tasks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
-const { findByIdAndUpdate } = require('./models/user')
+const { findByIdAndUpdate, translateAliases } = require('./models/user')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -142,6 +142,25 @@ app.get('/tasks/:id', async (req, res) => {
     // }).catch((e) => {
     //     res.status(500).send()
     // })
+})
+
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['desc', 'completed']
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidUpdate) {
+        return res.status(404).send({ 'error': 'invalid update' })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            res.send(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send()
+    }
 })
 
 app.listen(port, () => {
