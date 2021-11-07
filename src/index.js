8f992d789ec48f6872094a9211ade3bd066916cc
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-07 23:51:50 +0000

    Created DELETE endpoint for tasks

--- src/index.js
+++ src/index.js
@@ -176,6 +176,18 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 app.listen(port, () => {
     console.log('server is active on ' + port)
 })
