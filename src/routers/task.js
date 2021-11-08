0af0dbda8215c40e370e91afacdc60e7bda9e3e0
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-08 14:55:29 +0000

    Separate route files for User and Task using Express Router

---
+++ src/routers/task.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const router = new express.Router
+const Task = require('../models/task')
+
+
+// CREATE a task
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+    // task.save().then(() => {
+    //     res.status(201).send(task)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
+})
+
+// GET all tasks
+router.get('/tasks', async (req, res) => {
+
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+
+// GET task with ID
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            res.status(404).send()
+        }
+        res.status(200).send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+// UPDATE a task with ID
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['desc', 'completed']
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidUpdate) {
+        return res.status(404).send({ 'error': 'invalid update' })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!task) {
+            res.send(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+
+// DELETE a task with ID
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
