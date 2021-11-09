2c57754d77e2672bae11af763d5210172d7fa4d6
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-09 15:32:10 +0000

    created auth middleware to verify token

---
+++ src/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'teskkey')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        if (!user) {
+            throw new Error()
+        }
+        req.user = user
+        next()
+        console.log()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate perfectly' })
+    }
+}
+
+module.exports = auth
