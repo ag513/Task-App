85ced7b031649a80a717ccbdc8d716e49acae438
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-10 11:33:35 +0000

    removed route to get user with id

--- src/routers/user.js
+++ src/routers/user.js
@@ -60,29 +60,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-// GET user with ID
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404)
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
 // UPDATE a user with ID
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
