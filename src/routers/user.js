72107000b6eafb450be91b65f0ff1fd81823ed91
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-10 11:51:37 +0000

    Updated patch endpoint to check if user is authenticated and then update the user

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,7 +61,7 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 // UPDATE a user with ID
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
@@ -69,14 +69,9 @@ router.patch('/users/:id', async (req, res) => {
         res.status(400).send({ 'error': 'Invalid update' })
     }
     try {
-        const user = await User.findById(req.params.id)
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
 
     } catch (e) {
         res.status(400).send(e)
