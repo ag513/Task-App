0b465a345643913ff9e2ff58c3c00efffe18963a
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-10 11:21:54 +0000

    Hide password, tokens in the response sent back to the user

--- src/models/user.js
+++ src/models/user.js
@@ -49,6 +49,18 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+
+userSchema.methods.getPublicProfile = async function () {
+    const user = this
+    const userObject = user.toObject();
+
+    delete userObject.tokens
+    delete userObject.password
+
+    return userObject
+
+}
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({ _id: user._id.toString() }, 'teskkey')
