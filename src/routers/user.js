861fd0d0546287929899eba1f5e38f13f43124b1
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-11-15 23:59:39 +0000

    set file size. file filters and error handling

--- src/routers/user.js
+++ src/routers/user.js
@@ -95,11 +95,23 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            cb(new Error('file must be a image'))
+        }
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 
